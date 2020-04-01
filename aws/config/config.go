f61ca2a883b405cf0b48cd4860ae45ff80bcc7e5
Allison Maheu <allison.maheu@disneystreaming.com>
2020-04-01 09:44:59 -0400

    Added http timeouts and AWS client retry config

---
+++ aws/config/config.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/client"
+	"github.com/disneystreaming/go-ssmhelpers/util/httpx"
+)
+
+// NewDefaultConfig will return default AWS Configs that are meant to be
+// merged in. Luckily both the deprecated session.New() and session.NewSession()
+// take `cfgs ...*aws.Config` then merge them in.
+//
+// This means we can change our sessions to be `session.New(<whatver>, session.NewDefaultConfig())
+// If we need to override it we can swap order (last config's value wins)
+func NewDefaultConfig() *aws.Config {
+	return &aws.Config{
+		HTTPClient: httpx.NewDefaultClient(),
+		Retryer: &client.DefaultRetryer{
+			NumMaxRetries: 10,
+		},
+	}
+}
+
+// NewDefaultConfigWithRegion returns the same config as NewDefaultConfig, but allows a user to specify a region as well
+func NewDefaultConfigWithRegion(region string) *aws.Config {
+	return &aws.Config{
+		Region:     aws.String(region),
+		HTTPClient: httpx.NewDefaultClient(),
+		Retryer: &client.DefaultRetryer{
+			NumMaxRetries: 10,
+		},
+	}
+}
